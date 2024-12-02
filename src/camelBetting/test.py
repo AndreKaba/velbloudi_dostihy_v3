a8005566cb105592957ebbe0f279178e273fa00f
Ondřej Kába <ondrej.kaba@amitia-ai.com>
2024-12-02 13:16:13 +0100

    Add npc_battle and test_game

--- src/camelBetting/test.py
+++ src/camelBetting/test.py
@@ -1,9 +1,15 @@
 """Module containing various tests for the game entities."""
+import random
+
 from camelBetting.entities.board import Board
 from camelBetting.entities.move import DiceRoll, StonePut
 from camelBetting.simulation import Simulation
+from camelBetting.game import Game
+from camelBetting.entities.player import EvilNpc, RandomNpc, LessRandomNpc, AdequateNpc, RollerNpc, HumanPlayer
 
 import time
+from collections import defaultdict
+import numpy as np
 import cProfile
 
 
@@ -67,12 +73,52 @@ def test_approximation():
     # print(sum(outcomes.values()))
 
 
+def npc_battle():
+    players = [
+        RandomNpc('Silly Guy', threshold_for_overall_bets=8),
+        LessRandomNpc('Less Random Guy', threshold_for_overall_bets=8),
+        EvilNpc('Evil Guy', threshold_for_overall_bets=8, game_approx_number=5000),
+        AdequateNpc('Adequate Guy 3 moves', threshold_for_overall_bets=8, game_approx_number=5000, n_top_moves=3),
+        AdequateNpc('Adequate Guy 5 moves', threshold_for_overall_bets=8, game_approx_number=5000, n_top_moves=5),
+        RollerNpc('High Roller'),
+    ]
+    outcomes = defaultdict(list)
+    winners = defaultdict(int)
+    for i in range(100):
+        print(f"GAME {i}")
+        random.shuffle(players)
+        game = Game(players)
+        game.play()
+        print(f"Game {i} ended, outcomes: {game.board.current_player_order}")
+        for pl in game.board.current_player_order:
+            outcomes[pl[0]].append(pl[1])
+        winners[game.board.current_player_order[0][0]] += 1
+
+    print()
+    for player, scores in outcomes.items():
+        print(f"'{player}' mean score: {np.mean(scores)} (std: {np.std(scores)}, min: {np.min(scores)}, "
+              f"max: {np.max(scores)})")
+    print(dict(winners))
+
+
+def test_game():
+    players = [
+        LessRandomNpc('Less Random Guy', threshold_for_overall_bets=8),
+        HumanPlayer('okab.', show_evs=True, threshold_for_game_approx=8)
+    ]
+    game = Game(players)
+    game.play()
+
+    print(game.board.current_player_order)
+
 
 def main():
     s = time.time()
     # test_board_moves()
     # test_simulation()
-    test_approximation()
+    # test_approximation()
+    # npc_battle()
+    test_game()
     # cProfile.run('test_simulation()')
     print(time.time() - s)
 
